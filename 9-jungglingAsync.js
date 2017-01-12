1e04c969f9918f47ca9c0ba361fc13d9c811cbc6
trung.tran <trungtranthanh.k56@gmail.com>
2017-01-12 13:26:24 +0700

    process async by Promise

--- 9-jungglingAsync.js
+++ 9-jungglingAsync.js
@@ -13,13 +13,32 @@ urls.forEach((element, index)=> {
         response.on('data', (chunk) => {
             body += chunk;
         });
-        response.on('end', ()=> {arr[index]= body;
+        response.on('end', ()=> {
+            arr[index] = body;
             j++;
-            if(j==3){
+            if (j == 3) {
                 arr.forEach((element)=>(console.log(element)));
             }
         });
     }).on('error', console.error);
 })
 
+/*function asyncFunc(url) {
+ return new Promise(
+ function (resolve, reject) {
+ http.get(url, (response) => {
+ var body = '';
+ response.setEncoding('utf8');
+ response.on('error', console.error);
+ response.on('data', (chunk) => {
+ body += chunk;
+ });
+ response.on('end', ()=> {
+ resolve(body);
+ });
+ }).on('error', (err) => reject(error));
+ })
 
+ }
+ var allPromise = urls.map(asyncFunc);
+ Promise.all(allPromise).then((results)=> results.forEach((a)=>console.log(a)));*/
